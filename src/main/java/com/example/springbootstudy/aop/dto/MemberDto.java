f036ec691054d88c124edc9b8eacc115a3b4b25b
이경범 <tkdwk567@naver.com>
2022-07-25 14:37:18 +0900

    log aop skill study 1

---
+++ src/main/java/com/example/springbootstudy/aop/dto/MemberDto.java
@@ -0,0 +1,24 @@
+package com.example.springbootstudy.aop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * packageName    : com.example.springbootstudy.aop.dto
+ * fileName       : MemberDto
+ * author         : tkdwk567@naver.com
+ * date           : 2022/07/25
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MemberDto {
+
+    private Long id;
+    private int age;
+    private String name;
+}
